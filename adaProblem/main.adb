05dcca82b53be00add4232b7624183211f3d94d0
Norbert Jaśniewicz <norbert.jasniewicz@protonmail.com>
2023-01-25 00:04:27 +0100

    Dodano rozwiązanie w Adzie z ograniczonymi logami

--- adaProblem/main.adb
+++ adaProblem/main.adb
@@ -4,59 +4,57 @@ with Philosopher;
 use Philosopher;
 
 procedure main is
-  fork1 : ForkPtr;
-  fork2 : ForkPtr;
-  philo1 : PhilosopherPtr;
-  philo2 : PhilosopherPtr;
-  run1 : RunPhilo;
-  run2 : RunPhilo;
-  work : Boolean;
+  type philoNum is range 0..5;
+  type philoArr is array(philoNum) of PhilosopherPtr;
+  type forkArr is array(philoNum) of ForkPtr;
+  type threadArr is array(philoNum) of RunPhilo;
+  philoIdx : philoNum := 0;
+  idPool : Integer := 0;
+  philos : philoArr;
+  forks : forkArr;
+  work : Boolean := True;
+  maxMeals : Integer := 5;
+  threads : threadArr;
 begin
-  fork1 := new Fork;
-  fork1.id := 0;
-  --fork1.mtx := MyMutex;
-  fork2 := new Fork;
-  fork2.id := 1;
-  --fork2.mtx := MyMutex;
-  philo1 := new philosopher.Philosopher;
-  philo2 := new philosopher.Philosopher;
-  philo1.eatenMeals := 0;
-  philo1.id := 0;
-  philo1.leftFork := fork1;
-  philo1.righFork := fork2;
-  --philo1.mtx := MyMutex;
-  philo1.work := True;
-  philo2.eatenMeals := 0;
-  philo2.id := 1;
-  philo2.leftFork := fork2;
-  philo2.righFork := fork1;
-  --philo2.mtx := MyMutex;
-  philo2.work := True;
 
-  work := True;
-  run1.Start(ptr => philo1);
-  run2.Start(ptr => philo2);
+  for index in philoNum'Range loop
+    philos(index) := new philosopher.Philosopher;
+    philos(index).id := idPool;
+    philos(index).work := True;
+    philos(index).eatenMeals := 0;
+    forks(index) := new Fork;
+    forks(index).id := idPool;
+    idPool := idPool + 1;
+  end loop;
+
+  for index in philoNum'Range loop
+    philos(index).leftFork := forks(index);
+    if index = philoNum'Last then
+      philos(index).righFork := forks(0);
+    else
+      philos(index).righFork := forks(index + 1);
+    end if;
+  end loop;
+
+  for index in philoNum'Range loop
+    threads(index).Start(philos(index));
+  end loop;
 
   while work loop
     delay 1.0;
     work := False;
-    philo1.mtx.lock;
-    if philo1.eatenMeals < 5 then
-      work := True;
-    end if;
-    philo1.mtx.unlock;
-    philo2.mtx.lock;
-    if philo2.eatenMeals < 5 then
-      work := True;
-    end if;
-    philo2.mtx.unlock;
+    for index in philoNum'Range loop
+      philos(index).mtx.lock;
+      if philos(index).eatenMeals < maxMeals then
+        work := True;
+      end if;
+      philos(index).mtx.unlock;
+    end loop;
   end loop;
 
-  philo1.mtx.lock;
-  philo1.work := False;
-  philo1.mtx.unlock;
-  philo2.mtx.lock;
-  philo2.work := False;
-  philo2.mtx.unlock;
-
+  for index in philoNum'Range loop
+    philos(index).mtx.lock;
+    philos(index).work := False;
+    philos(index).mtx.unlock;
+  end loop;
 end main;
